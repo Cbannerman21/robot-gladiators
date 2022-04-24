6b0f2816b62f6298996f89ac6a226917e3507955
Cbannerman <cbannerman21@gmail.com>
2022-04-24 18:01:50 -0400

    added console logging

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function called fight
+var playerName = window.prompt("What is your robots name?");
+
+// Note the lack of quotation marks around the player name
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//what is this 
+console.log("Our robots name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight ();
