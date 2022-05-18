b5444aeb1d9051bc50e646024b6f8ce0c401389b
Cbannerman <cbannerman21@gmail.com>
2022-05-18 18:04:15 -0400

    added local storage for high scores

--- assets/js/game.js
+++ assets/js/game.js
@@ -142,10 +142,26 @@ var startGame = function() {
   var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
 
+    //check local storage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+      highScore = 0;
+    }
+    //if player has more money than the high score, player has a new high high score
+    if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+      alert(playerInfo.name + " did not beath the high score of " + highScore + ". Maybe Next time!");
+    }
     //if player is still alive, player wins!
     if (playerInfo.health > 0) {
       window.alert("Great job, you've survived the game! you now have a score of "+ playerInfo.money + ".");
     }
+    
     else {
       window.alert("You've lost your robot in battle.");
     }
